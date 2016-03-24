df9165941c2b489523553f5a12f687079c53811a
oz modic <syzygy.antithesis@gmail.com>
2016-03-23 22:43:10 -0600

    what the fuck am i doing with this init.d bullshit anyways
    added .gitignore
    removed tribality.init and tribality_daemon.rb from version control
    
    will put back in once i figure out what the fuck i'm doing

--- tribality_daemon.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'daemons'
- 
-pwd = Dir.pwd
-Daemons.run_proc('tribality_daemon.rb', {:dir_mode => :normal, :dir => "/var/run/"}) do
-	Dir.chdir(pwd)
-	exec "sudo rackup -D"
-end
