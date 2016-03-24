ce749758ce47d7d03043f2fe879e838c79b39c39
oz modic <syzygy.antithesis@gmail.com>
2016-03-24 15:10:13 -0600

    added shit and stuff sendmail doesn't work on my machine, so stores contacts in mongo

--- app.rb
+++ app.rb
@@ -3,9 +3,33 @@
 require 'sinatra'
 require 'tilt/erb'
 require 'haml'
+require 'mongo'
+require "mongo_mapper"
 
 class Tribality < Sinatra::Base
   set :port, 80
+  require 'net/smtp'
+
+  def send_email(to,opts={})
+    opts[:server]      ||= 'localhost'
+    opts[:from]        ||= 'no.fucking.clue@tribal.strangled.net'
+    opts[:from_alias]  ||= 'Tribal Mailer'
+    opts[:subject]     ||= "Tribality Contact from #{opts[:from_alias]}"
+    opts[:body]        ||= "Really? They forgot a message too?"
+
+    msg = <<END_OF_MESSAGE
+From: #{opts[:from_alias]} <#{opts[:from]}>
+To: <#{to}>
+Subject: #{opts[:subject]}
+
+#{opts[:body]}
+END_OF_MESSAGE
+
+    return Net::SMTP.start(opts[:server], 25, "tribality") do |smtp|
+      smtp.send_message msg, opts[:from], to
+    end
+  end
+
   get '/' do
 	haml :index
   end
@@ -14,6 +38,22 @@ class Tribality < Sinatra::Base
 		!e.match(/^\./)
 	}.sort
 	haml :screenshots
+  end
+  get '/contact' do
+	haml :contact
+  end
+  post '/contact' do
+	url = "127.0.0.1:27017"
+	mongo = Mongo::Client.new([url], :database => "tribality")
+        contact = mongo[:contact]	
+	params["time"] = Time.now.to_i
+	success = contact.insert_one(params)
+	send_email("syzygy.antithesis@gmail.com", {
+		:from => params["email"],
+                :from_alias => params["name"],
+                :body => params["msg"]
+        })
+	haml :contact, :locals => {:success => success}
   end
 
 end
